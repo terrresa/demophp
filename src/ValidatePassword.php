983ef41174d066e9fad420f60c1c7fe551c12da3
eleana <eleana_marchan@hotmail.com>
2019-06-09 10:37:45 -0500

    Agregado phpdoc

--- src/ValidatePassword.php
+++ src/ValidatePassword.php
@@ -1,15 +1,30 @@
 
- <?php  
-class ValidatePassword{
+ <?php
 
-const MIN_LENGTH = 6;
-const MAX_LEGTH = 20;
+ /**
+  * Clase de prueba para Eleanex y Devops Full Power
+  * @link https://en.wikipedia.org/wiki/Email_address
 
-public function validLength($password){
+  */
+class ValidatePassword
+{
+    /**
+     * Define el tamaño máximo
+     */
+    const MIN_LENGTH = 6;
+    const MAX_LEGTH = 20;
 
-    $passLength = strlen($password);
+    /**
+     * Valida el tamaño que el tamaño del password sea acorde a los parametros
+     * bla bla
+     * 
+     * @param string $password
+     * @return void
+     */
+    public function validLength($password)
+    {
+        $passLength = strlen($password);
 
-    return $passLength >= self::MIN_LENGTH && $passLength <= self:: MAX_LEGTH;
-    
- }
+        return $passLength >= self::MIN_LENGTH && $passLength <= self:: MAX_LEGTH;
+    }
 }
